6891f8311be82a0d0b3f19dcf4b7721903f75b85
Gonzalo De Varona <gonzalodevarona@Gonzalos-MacBook-Air.local>
2025-03-31 23:10:56 -0500

    feat: added support for zipkin logger

--- currency-exchange-service/src/main/java/com/gonza/microservices/currency_exchange_service/CurrencyExchangeController.java
+++ currency-exchange-service/src/main/java/com/gonza/microservices/currency_exchange_service/CurrencyExchangeController.java
@@ -1,5 +1,7 @@
 package com.gonza.microservices.currency_exchange_service;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,6 +10,8 @@ import org.springframework.web.bind.annotation.RestController;;
 
 @RestController
 public class CurrencyExchangeController {
+
+    private Logger logger = LoggerFactory.getLogger(CurrencyExchangeController.class);
 
     @Autowired
     private CurrencyExchangeRepository repository;
@@ -21,6 +25,9 @@ public class CurrencyExchangeController {
         @PathVariable String from,
         @PathVariable String to
     ){
+
+        logger.info("retrieveExchangeValue called with {} to {}", from, to);
+
         CurrencyExchange currencyExchange = repository.findByFromAndTo(from, to);
 
         if(currencyExchange == null){
